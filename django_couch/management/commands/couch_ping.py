0f03b875f7c6ab157a94e515c98f9c3d078e99d2
Alexey Loshkarev <elf2001@gmail.com>
2010-05-27 17:40:06 +0300

    check no database case

--- django_couch/management/commands/couch_ping.py
+++ django_couch/management/commands/couch_ping.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
@@ -34,31 +35,35 @@ class Command(BaseCommand):
         verbosity = int(options.get('verbosity'))
         
         workers = []
-        
+
+        if not len(args):
+            print "No database to ping"
+            sys.exit(1)
+            
         for db_key in args:
             if verbosity >= 2:
                 print "Using database", db_key
                 print "Settings data:", settings.COUCHDB[db_key]
 
             db = django_couch.db(db_key)
-            
+
             for row in django_couch.design_docs(db):
                 nop, view = row.id.split('/', 1)
-                
+
                 d = db[row.id]
 
                 for function in d['views']:
                     worker = Pinger(verbosity, db_key, view, function)
                     workers.append(worker)
                     worker.start()
-                
+
         for worker in workers:
             if verbosity > 1:
                 print "Waiting for worker %s (%s/%s @ %s)..." % (worker, worker.view_name, worker.func_name, worker.db_key)
             worker.join()
             if verbosity > 1:
                 print '  done'
-
+        
         
         
             
